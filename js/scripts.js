955ab25de180ce0f9ae01bdb9953b1dd7db1899f
TristanLobaugh <ttrader_atl@yahoo.com>
2016-03-23 16:16:42 -0400

    added bootstraps typeahead plugin
    added bootstraps typeahead plugin and used has the api pull the top 20
    movies and add that into an array that typeahead uses to give choices
    for the user. Then once selected the api sends a query to the server
    and then js places the image in the html.

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,20 @@ $(document).ready(function(){
 	var newHTML = "";
 	var baseURL = "https://api.themoviedb.org/3/";
 	var apiKey = "?api_key=e9ddb24aed6d48c4342303aba5269e28";
-
+	var moviesToSearch = [];
 	var nowPlaying = baseURL + "movie/now_playing" + apiKey;
 	var configURL = baseURL + "configuration" + apiKey;
 	var movieSearchURL;
+	var matches = [];
+	var substringRegex;
 
 	$.getJSON(configURL, function(configData){
 		imagePath = configData.images.base_url;
 		});
 
 	$("#searchButton").click(function(){
-		searchFor = $("#movieSearch").val();
+
+		searchFor = $("#searchInput").val();
 		console.log(searchFor);
 		movieSearchURL = baseURL + "search/movie" + apiKey + "&query=" + searchFor + "&page 1";
 		$.getJSON(movieSearchURL, function(movieData){
@@ -27,9 +30,8 @@ $(document).ready(function(){
 		});
 	});
 
-	
-	//CODE FOR GETTING TOP @) MOVIES NOW PLAYING
-	
+	//CODE FOR GETTING TOP 20 MOVIES NOW PLAYING
+
 	// $.getJSON(nowPlaying, function(movieData){
 	// 	var movieImage = movieData.results;
 	// 	$(movieData.results).each(function(){
@@ -39,28 +41,47 @@ $(document).ready(function(){
 	// 	$("#posters").html(newHTML);
 	// });
 
+//TYPEAHEAD STUFF
 
-	
+	//ASSIGN TOP 20 movies to SEARCH ARRAY
+	$.getJSON(nowPlaying, function(movieData){
+		var movieImage = movieData.results;
+		$(movieData.results).each(function(){
+			moviesToSearch.push(this.original_title);
+		});
+	});
 
+	var substringMatcher = function(strs) {
+	  return function findMatches(q, cb) {
+	   // var matches, substringRegex;
 
+	    // an array that will be populated with substring matches
+	    matches = [];
 
+	    // regex used to determine if a string contains the substring `q`
+	    substrRegex = new RegExp(q, 'i');
 
+	    // iterate through the pool of strings and for any string that
+	    // contains the substring `q`, add it to the `matches` array
+	    $.each(strs, function(i, str) {
+	      if (substrRegex.test(str)) {
+	        matches.push(str);
+	      }
+	    });
 
+	    cb(matches);
+	  };
+	};
 
-
+	$('#search-bar .typeahead').typeahead({
+	  hint: true,
+	  highlight: true,
+	  minLength: 1
+	},
+	{
+	  name: 'moviesToSearch',
+	  source: substringMatcher(moviesToSearch)
+	});
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	
