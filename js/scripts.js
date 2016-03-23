6ed28acea558c026633936fe67f89886254bca79
TristanLobaugh <ttrader_atl@yahoo.com>
2016-03-23 14:48:14 -0400

    added searchable feature
    using an input field allow user to add text that will go out and search
    for movie titles that meet the text entered

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	var searchFor;
 	var imagePath;
 	var newHTML = "";
 	var baseURL = "https://api.themoviedb.org/3/";
@@ -6,20 +7,37 @@ $(document).ready(function(){
 
 	var nowPlaying = baseURL + "movie/now_playing" + apiKey;
 	var configURL = baseURL + "configuration" + apiKey;
+	var movieSearchURL;
 
 	$.getJSON(configURL, function(configData){
 		imagePath = configData.images.base_url;
-		console.log(imagePath);
 		});
 
-	$.getJSON(nowPlaying, function(movieData){
-		var movieImage = movieData.results;
-		$(movieData.results).each(function(){
-			newHTML += "<div class='col-sm-3'><img src=" + imagePath + "w300" + this.poster_path + "'></div>"
-		});
+	$("#searchButton").click(function(){
+		searchFor = $("#movieSearch").val();
+		console.log(searchFor);
+		movieSearchURL = baseURL + "search/movie" + apiKey + "&query=" + searchFor + "&page 1";
+		$.getJSON(movieSearchURL, function(movieData){
+			var movieImage = movieData.results;
+			$(movieData.results).each(function(){
+				newHTML += "<div class=' movie-poster col-sm-3'><img src=" + imagePath + "w300" + this.poster_path + "'></div>"
+			});
 
-		$("#posters").html(newHTML);
+			$("#posters").html(newHTML);
+		});
 	});
+
+	
+	//CODE FOR GETTING TOP @) MOVIES NOW PLAYING
+	
+	// $.getJSON(nowPlaying, function(movieData){
+	// 	var movieImage = movieData.results;
+	// 	$(movieData.results).each(function(){
+	// 		newHTML += "<div class='col-sm-3'><img src=" + imagePath + "w300" + this.poster_path + "'></div>"
+	// 	});
+
+	// 	$("#posters").html(newHTML);
+	// });
 
 
 	
